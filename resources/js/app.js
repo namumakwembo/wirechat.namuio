b5a1d0c254d3114e4a113f5bb81732d099754ca9
Namu Makwembo <makwembonamu@gmail.com>
2025-04-20 13:49:22 +0300

    added window.HighlightScroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,9 @@ import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.e
 import HighlightScroll from 'highlight-scroll'
 
   // Optionally, attach Livewire and Alpine to the window for global access
-  window.Livewire = Livewire
-  window.Alpine = Alpine
+  window.Livewire = Livewire;
+  window.Alpine = Alpine;
+  window.HighlightScroll = HighlightScroll;
 // Provide your own defaults
 Alpine.plugin(HighlightScroll({
     offset: 120,
